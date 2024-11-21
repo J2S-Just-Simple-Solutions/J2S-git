1562734641f8863c62ff5e906090d76c82e10c59
Julien M <bankette@users.noreply.github.com>
2024-11-21 10:38:45 +0100

    Feature Add release management (#5)
    * [jgit] INIT  feature-add_release
    
    * Add release management + global refacto
    
    * Add release management
    
    * Add release management
    
    * Add release management

--- README.md
+++ README.md
@@ -21,6 +21,16 @@ alias jgit='/path-to-yourj2sgit-project/J2S-Git/jgit.sh'
 
 Restart your terminal.
 
+### prerequisites
+You should have 1 remote named `origin` that head to a J2S github repository on all your local projects.
+You must have git install on your local.
+You must have github client install on your local https://cli.github.com/
+
+Configure you gh environment
+```
+gh repo set-default
+```
+
 ## Usage
 jgit will be used in command line directly from your project folder.
 
@@ -52,6 +62,9 @@ git remote add your-own-remote git@github.com:your-own-remote/My-Project.git
 ### Command line options
 `jgit -h` to get help
 
-`jgit feature <ticket-ID>` will create a feature branch on your local, your remote and on J2S remote.
+`jgit feature start <ticket-ID>` will create a feature branch on your local and on J2S remote and will create an associated PR on github with NFR flag.
 
-`jgit hotfix <ticket-ID>` will create a hotfix branch on your local, your remote and on J2S remote.
+`jgit hotfix start <ticket-ID>` will create a hotfix branch on your local and on J2S remote and will create an associated PR on github with NFR flag.
+
+`jgit release start` will create, or checkout the existing, release branch on your local and create it on J2S remote if needed.
+
--- jgit.sh
+++ jgit.sh
@@ -9,35 +9,18 @@ own_remote="bankette"
 j2s_remote="origin"
 reference_branch="main"
 
-github_own_remote_name=$own_remote
-github_project_name="J2S-Akeneo"
-
-remotes=$( git remote -v )
-
 ####################################
 #
-#         GLOBAL VERIFICATIONS
+#        MANDATORY PARAMETERS
+# This section should be the same
+#         for all developers
 #
 ####################################
-
-if [[ $remotes != *$j2s_remote* ]]; then
-  echo "Please configure J2S remote as "$j2s_remote
-  exit 0
-fi
-
-if [[ $remotes != *$own_remote* ]]; then
-  echo "Please configure your own remote as "$own_remote
-  exit 0
-fi
-
-diff=$( git diff )
-
-if [[ -n $diff ]]; then
-  echo "Please commit or stash all changes before resuming"
-  git diff
-  exit 0
-fi
-
+prefix_PR="__PR__"
+prefix_commit="[jgit]"
+prefix_init_commit="$prefix_commit INIT"
+suffix_init_commit=""
+stash=false;
 
 ####################################
 #
@@ -45,20 +28,21 @@ fi
 #
 ####################################
 
-create_all_branches() {
-    echo "Checkout and update $reference_branch branch"
-    git checkout $reference_branch
-    git fetch $j2s_remote
-    git pull $j2s_remote $reference_branch
-    git checkout -B $branch
-    git push $j2s_remote $branch
-    git commit --allow-empty -m "[jgit INIT COMMIT $reference_branch]"
-    git push $own_remote $branch
-# Les pull request ne focntionne pas car le client ne permet pas de créé des PR entre deux remotes...
-#    gh pr create --title $feature_name --body "jira/"$feature_name --web
+exit_safe() {
+    if $stash; then
+        git stash pop
+    fi
+
+    if [[ $1 == 0 ]]; then
+        echo "/!\ Script finished in error! Be careful about your branch management on local."
+
+        exit $1
+    fi
+
+    exit $1
 }
 
-create() {
+feature_start() {
     branch_in_local=$( git branch --list ${branch} )
     branch_in_remote=$(git ls-remote --heads ${j2s_remote} ${branch})
 
@@ -72,12 +56,129 @@ create() {
         echo "Existe en local mais pas en distant"
         echo "Non géré pour le moment"
     elif [[ -z ${branch_in_local} ]] && [[ -z ${branch_in_remote} ]]; then
-    #    echo "N'existe pas"
-        create_all_branches
+        echo "Checkout and reset $reference_branch branch"
+        git checkout $reference_branch --quiet
+        git fetch $j2s_remote --quiet
+        git reset --hard $reference_branch --quiet
+        echo "Create pull request branch $branch_PR branch"
+        git checkout -B $branch_PR --quiet
+        git push $j2s_remote $branch_PR --quiet
+        echo "Create working branch $branch branch"
+        git checkout -B $branch --quiet
+        git commit --allow-empty -m "$prefix_init_commit $branch $suffix_init_commit" --quiet
+        git push --set-upstream $j2s_remote $branch --quiet
+        git branch -D $branch_PR --quiet
+        echo "Create pull request"
+        gh pr create --title $feature_name --body "https://justsimplesolutions.atlassian.net/browse/"$feature_name --base=$branch_PR --head=$branch --label "NFR"
     else
         echo "On est dans la Matrix"
     fi
 }
+
+release_start() {
+    if [[ $(git status --porcelain) ]]; then
+        echo "/!\ Local changes, cannot start release"
+        exit_safe 0;
+    fi
+
+    git checkout $reference_branch
+    git fetch origin
+    current_tag=$(git tag -l --sort=-creatordate | head -n 1)
+    echo "Current tag: ${current_tag}"
+    major=0
+    feature=0
+    minor=0
+
+    regex="([0-9]+).([0-9]+).([0-9]+)"
+    if [[ $current_tag =~ $regex ]]; then
+        major="${BASH_REMATCH[1]}"
+        feature="${BASH_REMATCH[2]}"
+        minor="${BASH_REMATCH[3]}"
+    else
+        echo "A tag must already exists (x.x.x format)"
+
+        exit_safe 0
+    fi
+    feature=$(echo $feature + 1 | bc)
+    future_tag="${major}.${feature}.${minor}"
+
+    branch=release/${future_tag};
+    existed_in_local=$(git branch --list ${branch})
+    existed_in_remote=$(git ls-remote --heads origin ${branch})
+
+    echo "Searching a branch naming: ${branch}"
+
+    if [[ ! -z ${existed_in_remote} ]]; then
+        if [[ ! -z ${existed_in_local} ]]; then
+            echo "Release ${branch} local branch exists, deletion..."
+            git branch -D ${branch}
+        fi
+        echo "Remote branch exists, use it..."
+        git checkout --track origin/${branch}
+    
+        return 1;
+    fi
+
+    echo "Release does not exists, create it..."
+    git reset --hard origin/$reference_branch
+    git checkout -b ${branch}
+    git commit --allow-empty -m "$prefix_init_commit release ${branch}. $suffix_init_commit"
+    git push origin ${branch}
+}
+
+release_merge() {
+    release_start
+    branch=$branch_PR;
+    existed_in_local=$(git branch --list ${branch})
+    existed_in_remote=$(git ls-remote --heads origin ${branch})
+    release_branch=$(git rev-parse --abbrev-ref HEAD)
+    existed=0
+
+    echo "Searching a branch naming: ${branch}"
+
+    if [[ ! -z ${existed_in_local} ]]; then
+        echo "Feature branch exists on local machine, use it..."
+        existed=1
+        git merge --no-ff ${branch} -m "$prefix_commit Merge feature branch : $branch"
+    fi
+
+    if [[ ! -z ${existed_in_remote} && existed=0 ]]; then
+        echo "Remote branch exists, use it..."
+        existed=1
+        git merge --no-ff origin/${branch} -m "$prefix_commit Merge feature branch : $branch"
+    fi
+
+    if [[ $existed == 0 ]]; then
+        echo "/!\ Feature branch was not found!"
+
+        exit 0
+    fi
+
+   git push origin ${release_branch}
+}
+
+####################################
+#
+#         GLOBAL VERIFICATIONS
+#
+####################################
+remotes=$( git remote -v )
+
+if [[ $remotes != *$j2s_remote* ]]; then
+  echo "Please configure J2S remote as "$j2s_remote
+  exit_safe 0
+fi
+
+if [[ $(git status --porcelain) ]]; then
+    echo "You have uncommited modifications."
+    read -p "Do you want to stash and unstash changes at the end of process ? [y/n] " yn
+    echo
+    if [[ ! $yn =~ ^[Yy]$ ]]; then
+        exit_safe 0
+    fi
+    git stash save "[jGIT]"
+    stash=true;
+fi
 
 ################################################################################
 # Help                                                                         #
@@ -86,14 +187,17 @@ help()
 {
     echo "This script is used to manage feature or hotfix creation for development purpose."
     echo
-    echo "It will create a branch correctly named on local, on your own remote and on J2S remote as well."
+    echo "It will create branches correctly named on local and on J2S remote."
+    echo "It will create all needed branches to create a clean PR on Github."
+    echo "The PR will be created automatically by github client cli."
     echo
-    echo "Syntax: jgit [feature|hotfix] feature_name"
+    echo "For now, only new feature or hotfix creation is supported."
+    echo
+    echo "Syntax: jgit [feature|hotfix] start <feature_name>"
     echo "options:"
     echo "-h    Print this Help."
     echo
 }
-
 
 ####################################
 #
@@ -104,32 +208,59 @@ help()
 while getopts ':hs:' option; do
   case "$option" in
     h) help
-       exit 1
+       exit_safe 1
        ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
-       exit 1
+       exit_safe 1
        ;;
    \?) printf "illegal option: -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
-       exit 1
+       exit_safe 1
        ;;
   esac
 done
 
 if [[ $1 == "feature" ]] || [[ $1 == "hotfix" ]]; then
-    if [[ -z $2 ]]; then
-        echo "Please set a $1 name as second argument"
-        exit 0;
+    if [[ -z $3 ]]; then
+        echo "Please set a $1 name as third argument"
+        exit_safe 0;
     fi
-    feature_name=$2
+    feature_name=$3
     branch=$1-$feature_name
-
-    create;
+    branch_PR=$prefix_PR$branch
+    
+    if [[ -z $2 ]]; then
+        help
+    fi
+    if [[ $2 == "start" ]]; then
+        feature_start;
+    else
+        echo "argument $2 note supported"
+        exit_safe 0
+    fi
+elif [[ $1 == "release" ]]; then
+    if [[ -z $2 ]]; then
+        help
+    fi
+    if [[ $2 == "start" ]]; then 
+        release_start;
+    elif [[ $2 == "merge" ]]; then 
+        if [[ -z $3 ]]; then
+            echo "Please set a branch name as third argument"
+            exit_safe 0;
+        fi
+        branch_PR=$prefix_PR$3
+        release_merge;
+    else
+        echo "argument $2 note supported"
+        exit_safe 0
+    fi
 elif [[ $1 == "help" ]]; then
     help
-    exit 1;
+    exit_safe 1;
 else
     echo "argument $1 note supported"
-    exit 0
+    exit_safe 0
 fi
+exit_safe 1 ;
