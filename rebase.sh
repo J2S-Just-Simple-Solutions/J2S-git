03445d4fac0368b372aded3dbeb1b3cc0a77b77e
Julien M <bankette@users.noreply.github.com>
2025-02-20 12:01:17 +0100

    Add the management for reopened PR

--- rebase.sh
+++ rebase.sh
@@ -51,7 +51,7 @@ feature_rebase() {
         git checkout -b $branch_PR $j2s_remote/$branch_PR
     fi
 
-    git checkout $branch
+    git checkout $branch --quiet
 
     # Récupérer le dernier commit avec le pattern jgit
     last_init_commit=$(get_last_commit_with_pattern)
@@ -59,6 +59,47 @@ feature_rebase() {
         echo "Aucun commit trouvé avec le pattern jgit"
         exit_safe 1
     fi
+
+    #############################################################################
+    # Cette partie permet de gérer le cas où la PR avait déjà été validé et mergée
+    # puis réouverte par la suite, en effet dans ce cas là il y a déjà des commits 
+    # présents sur la branche PR qui ne doivent pas être oubliés
+    ##############################################################################
+    
+    
+    # Récupérer la liste des commits en avance sur branch_PR
+    local commits_in_advance_on_PR=($(git rev-list "$reference_branch..$branch_PR"))
+
+    # normalement le premier commit de la branche de travail et de la branche de PR doivent être les mêmes
+    # Si ce n'est pas les mêmes c'est pas normal on coupe le script.
+    if [[ "${commits_in_advance_on_PR[0]}" != $last_init_commit ]]; then
+        printf "%sLe premier commit de la branche %s%s%s est %s%s%s. Sur la branche %s%s%s il vaut %s%s%s, ce n'est pas normal%s\n" \
+        "$(tput setaf 2)"  \
+        "$(tput setaf 1)" "$branch_PR" "$(tput setaf 2)" \
+        "$(tput setaf 1)" "${commits_in_advance_on_PR[0]}" "$(tput setaf 2)" \
+        "$(tput setaf 1)" "$branch" "$(tput setaf 2)" \
+        "$(tput setaf 1)" "${last_init_commit[0]}" "$(tput setaf 2)" \
+        "$(tput sgr0)"
+
+        exit_safe 0
+    fi
+
+    # On est dans le cas d'une PR qui déjà été mergée puis réouverte, on affiche la liste complète pour bien la valider visuellement.
+    if [[ $commits_in_advance_on_PR -gt 2 ]]; then
+        printf "%sLes commits suivants sont présents sur la branche %s%s%s actuelle mais pas sur la branche %s%s%s. Il seront repris sur la future branche %s%s%s%s\n" \
+        "$(tput setaf 2)"  \
+        "$(tput setaf 1)" "$branch_PR" "$(tput setaf 2)" \
+        "$(tput setaf 1)" "$reference_branch" "$(tput setaf 2)" \
+        "$(tput setaf 1)" "$branch_PR" "$(tput setaf 2)" \
+        "$(tput sgr0)"
+        for commit in "${commits_in_advance_on_PR[@]}"; do
+            get_commit_info "$commit"
+        done
+    fi
+
+    ###################################
+    # Gestion du rebase en cherrypick
+    ###################################
 
     # Lister les commits depuis le dernier commit d'init (dans l'ordre du plus ancien au plus récent)
     local commits=($(list_commits_since "$last_init_commit"))
@@ -84,9 +125,9 @@ feature_rebase() {
     git checkout $reference_branch --quiet
     git pull $j2s_remote $reference_branch --quiet
 
-    # rebase de la branche PR par application du commit d'init en cherry pick
+    # rebase de la branche PR par application des commit déjà présent sur l'ancienne branche PR en cherry pick
     checkout_or_create_branch $branch_PR_rebase
-    cherry_pick "$last_init_commit"
+    cherry_pick_commits "${commits_in_advance_on_PR[@]}"
 
     # rebase de la branche principal par application de tous les commits en cherry pick
     checkout_or_create_branch $branch_rebase
